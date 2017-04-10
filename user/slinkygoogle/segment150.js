73fe208c2b561f77b10dc0d99304273555379bda
Leroy Thompson <leroy.ron@gmail.com>
2017-04-09 20:58:37 -0400

    clean up

--- user/slinkygoogle/segment150.js
+++
@@ -1,8 +0,0 @@
-var Authority = new function (timeline, timeframe, buffer, binding) {
-    this.segmentID = 150
-
-    this.main = function () {
-        timeline.revertFromTo(840, this.segmentID)
-    }
-    return this
-}(this.ctx.timeline, this.ctx.timeline.addon.timeframe, this.ctx.timeline.addon.buffer, this.ctx.timeline.addon.binding)
