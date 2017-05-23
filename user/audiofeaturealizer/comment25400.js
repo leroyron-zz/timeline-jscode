338726bc420fb190dbc696c5c957f8e5343716a4
Leroy Thompson <leroy.ron@gmail.com>
2017-05-23 02:33:18 -0400

    dummy comment ref audiofeaturealizer demo

---
+++ user/audiofeaturealizer/comment25400.js
@@ -0,0 +1,8 @@
+window.Authority = new function (app, THREE, camera, canvas, ctx) {
+    this.actionID = 25400
+
+    this.main = function () {
+        // used for reference, because it may get skipped over by mp3 syncing
+    }
+    return this
+}(this.app, this.THREE, this.ctx.camera, this.canvas, this.ctx)
